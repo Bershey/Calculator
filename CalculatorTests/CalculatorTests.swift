7d60c62db35a6c80f27f0cb2da53453b76530176
minminmikan <ff.remon.9123@gmail.com>
2021-10-04 17:26:55 +0900

    テストコードを追記。

--- CalculatorTests/CalculatorTests.swift
+++ CalculatorTests/CalculatorTests.swift
@@ -10,24 +10,25 @@ import XCTest
 
 class CalculatorTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var calc = CalculatorLogic()
+
+    func testChengeSign() {
+        calc.setNumber(2)
+        let newValue = calc.calculate(symbol: "+/-")
+        XCTAssertEqual(newValue, -2)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testPercent() {
+        calc.setNumber(3)
+        let newValue = calc.calculate(symbol: "%")
+        XCTAssertEqual(newValue, 0.03)
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+    func testAllClear() {
+        calc.setNumber(4)
+        let newValue = calc.calculate(symbol: "AC")
+        XCTAssertEqual(newValue, 0)
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
     }
 
 }
