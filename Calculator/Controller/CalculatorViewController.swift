3bd22e6707970a0570615d2dcb271268bd85d771
minminmikan <ff.remon.9123@gmail.com>
2021-10-04 17:15:46 +0900

    四則演算の実装。

--- Calculator/Controller/CalculatorViewController.swift
+++ Calculator/Controller/CalculatorViewController.swift
@@ -9,9 +9,27 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
     // MARK: - IBOutlets
-
+    @IBOutlet weak var displayLabel: UILabel!
 
     // MARK: - Properties
+    private var isFinishedTypingNumber: Bool = true
+    private var calculator = CalculatorLogic()
+
+    private var displayValue: Double {
+           get {
+               guard let number = Double(displayLabel.text!) else {
+                   return 0
+               }
+               return number
+           }
+           set {
+               if String(newValue).hasSuffix(".0") {
+                   displayLabel.text = String(newValue.description.replacingOccurrences(of: ".0", with: ""))
+               } else {
+                   displayLabel.text = String(newValue)
+               }
+           }
+       }
 
 
     // MARK: - LifeCycle
@@ -20,8 +38,29 @@ class CalculatorViewController: UIViewController {
     }
 
     // MARK: - IBActions
+    @IBAction func numberButtonPressed(_ sender: UIButton) {
+        if let numValue = sender.currentTitle {
+                 if isFinishedTypingNumber {
+                     displayLabel.text =   calculator.checkedNumber(number: numValue)
+                     isFinishedTypingNumber = false
+                 } else {
+                     displayLabel.text! += calculator.checkdContainDotNumber(number: numValue, display: displayLabel.text!) ?? ""
+                 }
+             }
+         }
 
 
+    @IBAction func calculatorButtonPressed(_ sender: UIButton) {
+        isFinishedTypingNumber = true
+
+             calculator.setNumber(displayValue)
+
+             if let calcMethod = sender.currentTitle {
+                 if let result = calculator.calculate(symbol: calcMethod) {
+                     displayValue = result
+                 }
+             }
+    }
 
 }
 
--- Calculator/Mode/CalculatorLogic.swift
+++ Calculator/Mode/CalculatorLogic.swift
@@ -8,5 +8,67 @@
 import Foundation
 
 struct CalculatorLogic {
-    
+    // MARK: -Properties
+   private  var number: Double?
+
+    private var intermediateCalculation: (n1: Double, calcMethod: String)?
+
+    mutating func setNumber(_ number: Double) {
+        self.number = number
+    }
+
+  
+    mutating func calculate(symbol: String) -> Double? {
+        if let n = number {
+            switch symbol {
+            case "+/-":
+                return n * -1
+            case "AC":
+                return 0
+            case "%":
+                return n * 0.01
+            case "=":
+                return performTwoNumCalculation(n2: n)
+            default:
+                intermediateCalculation = (n1: n, calcMethod: symbol)
+            }
+        }
+        return nil
+    }
+
+    private func performTwoNumCalculation(n2: Double) -> Double? {
+
+        if let n1 = intermediateCalculation?.n1,
+           let operation = intermediateCalculation?.calcMethod {
+            switch operation {
+            case "+":
+                return n1 + n2
+            case "-":
+                return n1 - n2
+            case "×":
+                return n1 * n2
+            case "÷":
+                return n1 / n2
+            default:
+                break
+            }
+        }
+        return nil
+    }
+
+    func checkedNumber(number: String) -> String {
+        if number == "." {
+            return "0."
+        } else {
+            return number
+        }
+    }
+
+     func checkdContainDotNumber(number: String, display: String) -> String?  {
+        if number == "." && display.contains(".") {
+            return nil
+        }
+        return number
+    }
+
 }
