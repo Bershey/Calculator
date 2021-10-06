4723101273ef892f0489aa4c5073088d66e9c65f
minminmikan <ff.remon.9123@gmail.com>
2021-10-06 15:23:32 +0900

    UIテストを追加。

--- CalculatorUITests/CalculatorUITests.swift
+++ CalculatorUITests/CalculatorUITests.swift
@@ -9,4 +9,29 @@ import XCTest
 
 class CalculatorUITests: XCTestCase {
 
+    override func setUp() {
+        continueAfterFailure = false
+    }
+
+    func testNumberButtonPressed() {
+        let app = XCUIApplication()
+        app.launch()
+        let result = app.staticTexts["result"]
+        var numberArray: [XCUIElement] = []
+        for number in 0...9 {
+            let numberButton = app.buttons["\(number)"]
+            numberArray.append(numberButton)
+        }
+        let dotButton = app.buttons["dot"]
+        numberArray[0].tap()
+        numberArray[1].tap()
+        dotButton.tap()
+        numberArray[2].tap()
+        dotButton.tap()
+        for number in 3...9 {
+            numberArray[number].tap()
+        }
+        XCTAssertEqual(result.label, "1.23456789" )
+    }
+
 }
